0ea9e93d61fd826351dbb84e2e2ec893caa369ed
Meenakshi <Meenakshi.P@ust.com>
2022-11-03 19:48:47 +0530

    Collections_ArrayList

--- src/main/java/com/stackroute/collections/FruitsListService.java
+++ src/main/java/com/stackroute/collections/FruitsListService.java
@@ -1,22 +1,38 @@
 package com.stackroute.collections;
-
-import java.util.ArrayList;
-import java.util.List;
-
+import java.util.*;
 /*
  * This class contains methods for adding Fruits to a List and searching the fruits from the List
  */
 public class FruitsListService {
-
     public static List<String> addFruitsToList(String fruitNames) {
-        return null;
+        List<String> fruit_list = new ArrayList<>();
+        if(fruitNames==null||fruitNames.isEmpty()){
+            return fruit_list;}
+        else{
+            for(String fruit:fruitNames.split(","))
+            {if(!fruit.equals(fruit.toUpperCase()) && (!fruit_list.contains(fruit)) &&(!fruit.equals(fruit.toLowerCase())))
+                fruit_list.add(fruit);
+            }
+       }
+            return fruit_list;
     }
-
-    public static int searchFruitInList(List<String> fruitList, String searchFruit) {
-        return -2;
+    public static int searchFruitInList(List<String> fruit_list, String searchFruit) {
+        int i=0;
+        for(String fruit:fruit_list){
+            if(fruit.equals(searchFruit)){
+                return i;
+            }
+            i++;
+        }
+        return -1;
     }
-
-    public static int searchFruitInListIgnoreCase(List<String> fruitList, String searchFruit) {
-        return -2;
+    public static int searchFruitInListIgnoreCase(List<String> fruit_list, String searchFruit) {
+        int j=0;
+        for(String fruit:fruit_list){
+            if(fruit.equalsIgnoreCase(searchFruit)){
+                return j;
+            }j++;
+        }
+        return -1;
     }
 }
